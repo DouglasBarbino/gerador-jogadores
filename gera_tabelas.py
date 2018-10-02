e303b121ab736d2dfd3a5e05782600a0ded375fd
DouglasBarbino <douglas-448@hotmail.com>
2018-10-01 23:28:18 -0300

    Novas tabelas e funcionalidades
    - Adicionado regulamentos para torneios com 11 e 13 times
    - Embaralha os times inseridos, dando prioridade pra metade superior
    - Adicionado sistema de turnos

--- gera_tabelas.py
+++ gera_tabelas.py
@@ -24,17 +24,38 @@ dataTeams = dataTeams.flatten()
 #Leitura do arquivo JSON
 with open('chaveamento_jogos.json') as data_file:
     dataLeague = json.load(data_file)
-    
+
+#Embaralha os times caso desejado, permitindo que se divida no meio para que os times da metade
+#superior da lista continuem tendo um jogo a mais em casa em campeonatos com numero de times par
+#if (sys.argv[2] = 'y'):
+#Realiza a divisao, com shuffleTeams sendo apenas uma copia do vetor, entao as mudancas aqui afetam o vetor dataTeams
+breakTeams = np.array_split(dataTeams, 2)
+#Embaralha cada metade
+for teams in breakTeams:
+    np.random.shuffle(teams)
+#Embaralha o vetor como um todo
+#elif (sys.argv[2] = 'a'):
+#    np.random.shuffle(dataTeams)
+
 #Contador de rodadas
 numberMatchday = 0
-
-#Leitura das rodadas do torneio com aquele numero de clubes
-for rodadas in dataLeague[str(dataTeams.size)]:
-    #Incrementa o contador de rodadas
-    numberMatchday += 1
-    #Imprime qual rodada eh para facilitar a visualizacao
-    print("\nRodada " + str(numberMatchday))
-    #Percorre o numero de partidas que ocorrerao, sendo o maior inteiro menor que (numeroClubes / 2)
-    for i in range(0, math.floor(dataTeams.size/2)):
-        #Imprime a partida conforme o que foi lido na rodada
-        print(dataTeams[rodadas[2*i]] + " x " + dataTeams[rodadas[2*i+1]])
+#Numero de turnos
+numberTurns = 4
+#Repete a geracao de rodadas conforme o numero de turnos
+for turn in range(0, numberTurns):
+    #Leitura das rodadas do torneio com aquele numero de clubes
+    for rodadas in dataLeague[str(dataTeams.size)]:
+        #Incrementa o contador de rodadas
+        numberMatchday += 1
+        #Imprime qual rodada eh para facilitar a visualizacao
+        print("\nRodada " + str(numberMatchday))
+        #Percorre o numero de partidas que ocorrerao, sendo o maior inteiro menor que (numeroClubes / 2)
+        for i in range(0, math.floor(dataTeams.size/2)):
+            #Inverte a ordem dos times caso o numero de turnos restantes seja par, 
+            #permitindo que no ultimo turno os clubes da metade superior tenham um jogo a mais em casa 
+            if (((numberTurns - turn) % 2) == 0):
+                #Imprime a partida conforme o que foi lido na rodada
+                print(dataTeams[rodadas[2*i+1]] + "\tx\t" + dataTeams[rodadas[2*i]])
+            else:
+                #Imprime a partida conforme o que foi lido na rodada
+                print(dataTeams[rodadas[2*i]] + "\tx\t" + dataTeams[rodadas[2*i+1]])
